1ef363c3b3bc917c1afd44e3752012203e5db236
Richard Schoen <richard@richardschoen.net>
2023-05-09 08:53:48 -0500

    Create crtlabusr.cmd

---
+++ source/crtlabusr.cmd
@@ -0,0 +1,5 @@
+             CMD        PROMPT('Create Git Lab User')
+             PARM       KWD(USER) TYPE(*CHAR) LEN(10) PROMPT('User +
+                          name')
+             PARM       KWD(LIB) TYPE(*CHAR) LEN(10) DFT(*USERNAME) +
+                          PROMPT('User name')
